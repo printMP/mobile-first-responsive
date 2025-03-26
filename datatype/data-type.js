6c884a30a4be11059dbd2dd79c491e556a7d253c
printMP <bonez7_2000@yahoo.com>
2025-03-26 18:20:19 -0500

    Added data type HTML and JS in its own folder to show data types

---
+++ datatype/data-type.js
@@ -0,0 +1,22 @@
+let codeOutput = document.getElementById('code-output');
+console.log("The output is ",codeOutput);
+
+let hello = 'hello world!';
+console.log("This data type is a ",typeof hello);
+
+let number = 38;
+console.log("This data type is a ",typeof number);
+
+let bool = false;
+console.log("This is a ", typeof bool);
+
+let nul;
+console.log("This is a ", typeof nul);
+
+let obj = {"name":"John"}
+console.log("This is a ", typeof obj);
+
+let myArr = '[{"name":"Marvin"},{"name":"John"}]'
+console.log("This is a ", typeof myArr);
+
+codeOutput.innerHTML = 'check in console'
