dffe7079763abc952c7b7f73c20216d00d1a395a
printMP <bonez7_2000@yahoo.com>
2025-03-26 17:34:05 -0500

    Added script.js and edited style.css and pushed it

---
+++ script.js
@@ -0,0 +1,31 @@
+// referencing the ID named code-output in HTML
+let output = document.getElementById("code-output");
+
+// Printing the value of the output
+console.log("ouput =", output)
+
+// Declaration and initialization of the variable x with the value Morrison
+let x = "Morrison";
+
+// Declaration and initialization of the variable y with the value 3
+let y = 3;
+
+// Declaration and initialization of the variable z with the boolean value TRUE
+let z = true;
+
+x = "john";
+
+// Permanently setting the variable name to the value Jack
+const name = "Jack";
+
+// Unable to change name variable (as a test)
+// name = "Jason";
+
+// declaration of age
+let age;
+
+// initialization of age
+age = 33;
+
+output.innerHTML = x;
+
